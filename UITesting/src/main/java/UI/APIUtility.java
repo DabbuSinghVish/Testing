c91810a745d1f9691a6cd05c21c475c93957dc0f
Dabbu <dsvish92@gmail.com>
2024-11-26 00:02:32 +0530

    New project

---
+++ UITesting/src/main/java/UI/APIUtility.java
@@ -0,0 +1,30 @@
+package UI;
+
+public class APIUtility {
+	
+	
+	
+	public static void ValidateStatus(String str)
+
+	{
+
+		
+		
+		System.out.println(str);
+		
+		
+	}
+	
+	
+	public static void main(String args[])
+	
+	{
+		APIUtility.ValidateStatus("Dabbu");
+		
+		
+	}
+	
+	
+	
+
+}
