dd0cbdd506eed51d8a8c18f032505c9492558bdc
Dabbu <dsvish92@gmail.com>
2024-11-26 00:19:09 +0530

    good

--- UITesting/src/main/java/UI/APIUtility.java
+++ UITesting/src/main/java/UI/APIUtility.java
@@ -7,20 +7,15 @@ public class APIUtility {
 	public static void ValidateStatus(String str)
 
 	{
-
-		
-		
-		System.out.println(str);
-		
+	
+		System.out.println(str);	
 		
 	}
 	
 	
 	public static void main(String args[])
-	
 	{
-		APIUtility.ValidateStatus("Dabbu");
-		
+		APIUtility.ValidateStatus("Dabbu");	
 		APIUtility.ValidateStatus("Dabbu1");
 	}
 	
